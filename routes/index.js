9e0aba625818aae9b8908a9ac1319c94bea6ff27
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-07-12 17:52:47 -0700

    added Pizza api routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
+// Import the API routes from /api/indes.js 
+const apiRoutes = require('./api');
 const htmlRoutes = require('./html/html-routes');
 
+router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
 
 router.use((req, res) => {
